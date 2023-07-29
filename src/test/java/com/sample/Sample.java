0244e8cff9e2975756c7120d586e91a2c396d0b1
ananthpriyan <125973589+ananthpriyan@users.noreply.github.com>
2023-07-29 16:41:29 +0530

    Update Sample.java

--- src/test/java/com/sample/Sample.java
+++ src/test/java/com/sample/Sample.java
@@ -4,6 +4,8 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hi Master");
 		System.out.println("Hi Raj");
+		System.out.println("Hi i need anand and abhilash");
+		
 		
 	}
 
